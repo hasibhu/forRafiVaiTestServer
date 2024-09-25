f85440381f2dc126897be730a4e1f1c85a1932d1
hasibhu <hasibul1987@gmail.com>
2024-09-25 13:01:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,128 @@
+require("dotenv").config();
+const cookieParser = require("cookie-parser")
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const { MongoClient } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const dbConnect = async () => {
+    try {
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+dbConnect();
+
+
+
+const usersCollection = client.db('nextJsAuthentication').collection('users');
+
+
+
+//     // User Registration
+    app.post("/api/v1/register", async (req, res) => {
+      const { username, email, password } = req.body;
+
+      // Check if email already exists
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exist!!!",
+        });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Insert user into the database
+      await usersCollection.insertOne({
+        username,
+        email,
+        password: hashedPassword,
+        role: "user",
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully!",
+      });
+    });
+
+
+//     // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      // Find user by email
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign(
+        { email: user.email, role: user.role },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.EXPIRES_IN,
+        }
+      );
+
+     res.cookie('token', token, {
+    httpOnly: true, // Prevents JavaScript access to the cookie
+    secure: process.env.NODE_ENV === 'production', // Set to true in production
+    sameSite: 'Lax', // Protect against CSRF
+    maxAge: 86400 * 1000, // 1 day in milliseconds
+  });
+
+      res.json({
+        success: true,
+        message: "User successfully logged in!",
+        accessToken: token,
+      });
+    });
+
+//    
+
+
+// Test route
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  };
+  res.json(serverStatus);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// nextJsAuthentication
+
+// 77FtfX25kRFXZmj0
